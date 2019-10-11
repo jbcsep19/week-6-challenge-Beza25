2fd5421fc1311749bc851a5420c41558eb18cf3e
Beza25 <sirak.bezawit@gmail.com>
2019-10-11 10:01:52 -0400

    home page car list good!

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -26,6 +27,11 @@ public class HomeController {
         model.addAttribute("categories", categoryRepository.findAll());
         model.addAttribute("car", new Cars());
         return "carForm";
+    }
+    @PostMapping("/process_car")
+    public String processCar(@ModelAttribute Cars cars ){
+        carsRepository.save(cars);
+        return "redirect:/";
     }
 
 }
